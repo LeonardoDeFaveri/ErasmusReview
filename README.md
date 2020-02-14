5753123201d71573543cb929acfc296bd644f41f
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2020-02-14 17:50:35 +0100

    File del progetto

--- README.md
+++
@@ -1,2 +0,0 @@
-# ErasmusAdvisor
-Progetto per la recensione degli aspetti legati alle esperieze di PCTO ed Erasmus
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once "Controller/Controller.php";
-    
-    $controllo = new Controller();
-    $controllo->invoca();
-?>
