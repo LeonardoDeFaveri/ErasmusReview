23a307ffa31018c54f5e0a41f0f1803685a46777
RickyPv <61282435+RickyPv@users.noreply.github.com>
2020-02-21 12:49:18 +0100

    aggiunto costruttore e funzione verificaLogin

--- Model/Modello.php
+++ Model/Modello.php
@@ -7,8 +7,23 @@
     include_once "Aspetto.php";
     include_once "Valutazione.php";
     include_once "SchedaValutazione.php";
-    
+    include_once "ConnessioneDB/ConnessioneDB.php";    
+
     class Modello{
+        private $connessioneDB;
+        
+        public function __construct() {
+            $this->$connessioneDB = new ConnessioneDB();
+        }
+
+        public function verificaLogin(){
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $verificato = $connessioneDB->verificaCredenziali($email, $password);
+            return $verificato;
+        }
         
     }
+
+    
 ?>
