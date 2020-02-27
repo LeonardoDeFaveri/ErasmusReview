0bf95c8a33b68ae7fe7da69960b91deccbebde25
Riccardo <riccardo.pavetta@iisvittorioveneto.it>
2020-02-27 14:19:39 +0100

    aggiunta paginaStudente per provare il caricamento di un file

---
+++ View/paginaStudente.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    paginaStudente contiene una lista di tutte le esperienze svolte e attive per uno studente
+
+*/
+    include_once "include/struttura.php";
+
+    $html = creaHeader("Esperienze");
+    $html.=<<<testo
+        <p>ciao</p>
+        
+    testo;
+
+    $html .= creaFooter();
+    echo $html;
+?>
