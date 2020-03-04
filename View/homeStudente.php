47e9bc9f4021cf457cf073de2c3f3bfe2adef2a5
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2020-03-04 17:28:40 +0100

    prova

--- View/homeStudente.php
+++ View/homeStudente.php
@@ -4,6 +4,7 @@
  */
 if(session_id() == ''){
     session_start();
+    $_SESSION['root'] = __DIR__ . "/../";
 }
 include_once "{$_SESSION['root']}/View/include/struttura.php";
 include_once "{$_SESSION['root']}/Model/Soggetti/Studente.php";
