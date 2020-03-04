b59220a4314d2df932fdd9bc2ea5c9f1c875eee6
Riccardo <riccardo.pavetta@iisvittorioveneto.it>
2020-03-04 17:06:56 +0100

    sistemata homeStudente

--- View/homeStudente.php
+++ View/homeStudente.php
@@ -22,40 +22,40 @@ if(isset($_GET['errore']) || !isset($_SESSION['studente'])){
 
     if(isset($_SESSION['esperienze'])){
         $esperienze = unserialize($_SESSION['esperienze']);
-        $html.=<<<testo
-            <table id="esperienze">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Azienda</th>
-                        <th>Agenzia</th>
-                        <th>Famiglia</th>
-                        <th>Data inizio</th>
-                        <th>Data fine</th>
-                    </tr>
-                </thead>
-                <tbody>
-        testo;
+        
+        $html.="<p>in corso:</p>\n<br>\n";
+        $primoRiquadro = true;
 
         foreach ($esperienze as $esperienza) {
             $agenzia = $esperienza->getAgenzia() != NULL ? $esperienza->getAgenzia()->getNome() : "";
             $famiglia = $esperienza->getFamiglia() != NULL ? $esperienza->getFamiglia()->getCognome() : "";
-            $html .=<<<testo
-                    <tr>
-                        <td>{$esperienza->getId()}</td>
-                        <td><a href="#">{$esperienza->getAzienda()->getNome()}</a></td>
-                        <td><a href="#">{$agenzia}</a></td>
-                        <td><a href="#">{$famiglia}</a></td>
-                        <td>{$esperienza->getDal()}</td>
-                        <td>{$esperienza->getAl()}</td>
-                    </tr>
-            testo;
+            if($esperienza->getAl() >= date('Y-m-d')){
+                
+                $html.=<<<testo
+                <div class='riquadroEsperienza'>
+                    <a href='?comando=azienda-{$esperienza->getAzienda()->getId()}'>{$esperienza->getAzienda()->getNome()}</a>
+                    <p>dal:{$esperienza->getDal()} al {$esperienza->getAl()}</p>
+                    <a href='?comando=famiglia-{$esperienza->getFamiglia()->getId()}'>{$famiglia} {$esperienza->getFamiglia()->getNome()}</a><br>
+                    <a href='?comando=agenzia-{$esperienza->getAgenzia()->getId()}'>{$agenzia}</a><br>
+                    <a href='?comando=esperienza-{$esperienza->getId()}'>visualizza esperienza</a><br>
+                </div>\n
+                testo;
+            }else{
+                if($primoRiquadro){ //se è la prima esperienza che viene stampata scrivo il paragrafo sennò non serve perchè è gia stato scritto 
+                    $html.="<p>completate:</p>\n";
+                    $primoRiquadro = false;
+                }
+                $html.=<<<testo
+                <div class='riquadroEsperienza'>
+                    <a href='?comando=azienda-{$esperienza->getAzienda()->getId()}'>{$esperienza->getAzienda()->getNome()}</a>
+                    <p>dal:{$esperienza->getDal()} al {$esperienza->getAl()}</p>
+                    <a href='?comando=famiglia-{$esperienza->getFamiglia()->getId()}'>{$famiglia} {$esperienza->getFamiglia()->getNome()}</a><br>
+                    <a href='?comando=agenzia-{$esperienza->getAgenzia()->getId()}'>{$agenzia}</a><br>
+                    <a href='?comando=esperienza-{$esperienza->getId()}'>visualizza esperienza</a><br>
+                </div>\n
+                testo;
+            }
         }
-
-        $html .=<<<testo
-                </tbody>
-            </table>
-        testo;
     }
 }
 $html .= creaFooter();
