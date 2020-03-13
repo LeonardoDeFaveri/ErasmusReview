5240893ed8e0d9bebf711750914b20b57972680b
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2020-03-13 11:52:42 +0100

    Eliminata classe Admin.php e completato nel controller il case relativo alla homeAdmin

--- View/homeAdmin.php
+++ View/homeAdmin.php
@@ -5,15 +5,16 @@ if(session_id() == ''){
 }
 
 $html = creaHeader("Amministrazione");
-if(isset($_GET['errore']) || !isset($_SESSION['admin'])){
+if(isset($_GET['errore']) || !isset($_SESSION['email_utente'])){
     $html .= creaBarraMenu("");
     $html .=<<<testo
         <h2>Devi aver eseguito l'accesso come admin per poter vedere questa pagina</h2>
         <a href="login.php">Accedi</a>
     testo;
 }else{
-    $admin = unserialize($_SESSION['admin']);
-    $html .= creaBarraMenu($admin->getEmail());
+    $admin = $_SESSION['email_utente'];
+    $html .= creaBarraMenu($admin);
+    
 }
 $html .= creaFooter();
 echo $html;
