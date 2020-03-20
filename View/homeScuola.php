a18f53569a085cda2ebde451427e42dfab0c58bf
Leonardo <leonardo.defaveri01@gmail.com>
2020-03-20 11:18:01 +0100

    Corretto errore nella pagina homeScuola.php

--- View/homeScuola.php
+++ View/homeScuola.php
@@ -4,6 +4,8 @@ if(session_id() == ''){
     $_SESSION['root'] = __DIR__ . '/../';
 }
 include_once "{$_SESSION['root']}/Model/Soggetti/Scuola.php";
+include_once "{$_SESSION['root']}/Model/Classe.php";
+include_once "{$_SESSION['root']}/Model/Soggetti/Docente.php";
 include_once "{$_SESSION['root']}/View/include/struttura.php";
 
 $html = creaHeader("Amministrazione Scuola");
@@ -16,9 +18,8 @@ if(isset($_GET['errore']) || !isset($_SESSION['scuola'])){
 }else{
     $scuola = unserialize($_SESSION['scuola']);
     $html .= creaBarraMenu($scuola->getEmail());
-    if(isset($_SESSION['classi'])){
-        $classi = unserialize($_SESSION['classi']);
-    }
+    $classi = unserialize($_SESSION['classi']);
+    $docenti = unserialize($_SESSION['docenti']);
 }
 $html .= creaFooter();
 echo $html;
