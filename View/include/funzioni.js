3893519068baae76898dc95fd7ae0b42c4686727
ale <alessandro.rizzo@iisvittorioveneto.it>
2020-03-20 11:16:25 +0100

    aggiunto funz js

---
+++ View/include/funzioni.js
@@ -0,0 +1,10 @@
+function mostraMenuUtente(){
+    html="<div id=\"menuUtente\">\n\
+    <a href=\"../../index.php?comando=logout\">Esci</a><br>\n\
+    <a href=\"../../index.php?comando=gestione-account\">Gestione Account</a>\n\
+    </div>";
+    document.getElementById("utente").innerHTML = html;
+}
+    
+
+
--- View/include/struttura.php
+++ View/include/struttura.php
@@ -1,6 +1,7 @@
 <?php
+
 function creaHeader($nomePagina) {
-    $html =<<<testo
+    $html = <<<testo
     <!DOCTYPE html>  
     <html>
     <head>
@@ -9,6 +10,7 @@ function creaHeader($nomePagina) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="template/stile.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <script src="include/funzioni.js"></script>
     </head>
     <body>
         <div id="barra-superiore">
@@ -22,19 +24,39 @@ function creaHeader($nomePagina) {
 function creaBarraMenu($emailUtente) {
     $ricerca = creaSezioneRicerca();
     $utente = creaSezioneUtente($emailUtente);
+<<<<<<< HEAD
+    $html = <<<testo
+        <div id="barra-menu">
+    {$menu}
+    {$ricerca}
+    {$utente}    
+=======
     $html =<<<testo
             <div id="barra-menu">
         {$ricerca}
         {$utente}    
             </div>
+>>>>>>> eb02466e2148dd6b76b252170b4c0913b6fb79fb
         </div>
         <main>\n
     testo;
     return $html;
 }
 
+<<<<<<< HEAD
+function creaSezioneMenu() {
+    $html = <<<testo
+            <nav id="menu-principale">
+                <p>Menu</p>
+            </nav>\n
+    testo;
+    return $html;
+}
+
+=======
+>>>>>>> eb02466e2148dd6b76b252170b4c0913b6fb79fb
 function creaSezioneRicerca() {
-    $html =<<<testo
+    $html = <<<testo
             <div id="ricerca">
                 <form method="POST" action="../index.php?comando=cerca">
                     <input type="text" placeholder="Cerca.." name="cerca">
@@ -46,17 +68,23 @@ function creaSezioneRicerca() {
 }
 
 function creaSezioneUtente($email) {
-    $html =<<<testo
-            <div id="utente">
-                <p>{$email}</p>
-                <i id="icona-utente" class="material-icons">account_circle</i>
-            </div>\n
-    testo;
+    $html = "<div id=\"utente\">";
+    /*se l email è settata allora l accesso è stato effettuato*/
+    if(isset($email)){
+        /*allora posso cliccare e vedere i link a esci o gestione account*/
+        $html.="<p>{$email}</p>";
+        $html.="<i id=\"icona-utente\" class=\"material-icons\" onClick=\"mostraMenuUtente()\">account_circle</i>";
+    }else{
+        /*altrimenti no*/
+        $html.="<i id=\"icona-utente\" class=\"material-icons\">account_circle</i>";
+    }
+    $html .="</div>\n";
+            
     return $html;
 }
 
 function creaFooter() {
-    $html =<<<testo
+    $html = <<<testo
         </main>
         <footer>
             <address>Progetto: Erasmus Advisor</address>
@@ -67,4 +95,5 @@ function creaFooter() {
     testo;
     return $html;
 }
+
 ?>
