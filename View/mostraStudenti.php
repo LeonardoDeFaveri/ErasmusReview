dc0203c71c770eb3da997a9730fa3445c269f41b
Leonardo <leonardo.defaveri01@gmail.com>
2020-03-27 18:57:04 +0100

    Proseguito con mostraStudenti.php

--- View/mostraStudenti.php
+++ View/mostraStudenti.php
@@ -7,4 +7,40 @@ include_once "{$_SESSION['root']}/Model/Soggetti/Studente.php";
 include_once "{$_SESSION['root']}/View/include/struttura.php";
 
 $html = creaHeader("Tutti gli studenti");
+$html .= creaBarraMenu($_SESSION['email_utente']);
+$studenti = unserialize($_SESSION['studenti']);
+
+$html .=<<<testo
+    <h2>Tutti gli studenti</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Cognome</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Data di nascita</th>
+                <th>Azione</th>
+            </tr>
+        </thead>
+        <tbody>\n
+testo;
+
+foreach ($studenti as $studente) {
+    $html .=<<<testo
+            <tr>
+                <td>{$studente->getCognome()}</td>
+                <td>{$studente->getNome()}</td>
+                <td>{$studente->getEmail()}</td>
+                <td>{$studente->getDataNascita()}</td>
+            </tr>\n
+    testo;
+}
+
+$html .=<<<testo
+        </tbody>
+    </table>
+testo;
+
+$html .= creaFooter();
+echo $html;
 ?>
