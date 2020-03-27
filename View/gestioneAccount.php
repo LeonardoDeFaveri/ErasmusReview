9aac55e2976a6e271d8857cac8d8a4e84f27a6a7
ale <alessandro.rizzo@iisvittorioveneto.it>
2020-03-27 12:56:31 +0100

    case con tipi utente

--- View/gestioneAccount.php
+++ View/gestioneAccount.php
@@ -8,6 +8,22 @@ include_once "{$_SESSION['root']}/View/include/struttura.php";
 $html = creaHeader("Gestione Account");
 $html = $html . creaBarraMenu($_SESSION["email_utente"]);
 
+
+$soggetto=unserialize($_SESSION[$_SESSION["tipo_utente"]]);
+
+switch($_SESSION["tipo_utente"]){
+    case "azienda":
+        break;
+    case "agenzie":
+        break;
+    case "studente":
+        break;
+    case "docente":
+        break; 
+    case "admin":
+        break;
+}
+
 $html.=<<<testo
     <form method="POST" action="?cambio-password" onsubmit="return controlloCorrispondezaPassword(this)">
         <fieldset>
@@ -26,6 +42,8 @@ if(isset($_GET["errore"])){
         $html.="<p>Errore generico, non sono riuscito a cambiare la password</p>";
     }
 }
+
+
 
 $html.=creaFooter();
 echo $html;
