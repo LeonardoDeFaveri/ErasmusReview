b9d4f5ce7e0ef76de50ece1e6e770808d01efa03
Leonardo <leonardo.defaveri01@gmail.com>
2020-04-03 18:42:37 +0200

    Rimossa libreria jQuery

--- View/include/struttura.php
+++ View/include/struttura.php
@@ -10,7 +10,6 @@ function creaHeader($nomePagina) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="template/stile.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="include/funzioni.js"></script>
     </head>
     <body>
