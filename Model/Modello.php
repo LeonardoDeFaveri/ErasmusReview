fcbcebcb5528a168386a9551b2f08e4a9735ab68
Leonardo <leonardo.defaveri01@gmail.com>
2020-04-03 11:54:37 +0200

    Rimossa query update

--- Model/Modello.php
+++ Model/Modello.php
@@ -810,24 +810,6 @@ class Modello {
         $ris = $this->connessione->query($query);
         return $ris;
     }
-
-    public function update($tabella, $arrayCampi, $arrayValori, $arrayClausole) {
-        $query = "UPDATE $tabella SET ";
-        for ($i = 0; count($arrayCampi); $i++) {
-            $query .= $arrayCampi[$i] . "=" . $arrayValori[$i];
-            $query .= ", ";
-        }
-        $query = substr($query, 0, (strlen($query) - 2));
-
-        $query .= ") VALUES (";
-        foreach ($arrayValori as $elemento) {
-            $query .= "$elemento, ";
-        }
-        $query = substr($query, 0, (strlen($query) - 2));
-
-        $valore = $this->connessione->query($query);
-        return $valore;
-    }
     
     public function modificaPassword($digest) {
         $query = "UPDATE utenti SET password='{$digest}' WHERE email='{$_SESSION["email_utente"]}'";
